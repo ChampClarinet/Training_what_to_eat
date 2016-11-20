ad91f39744f6c96e3b96de2b993380d6da6d7f06
Wallop Opasakhun <champbassist159@gmail.com>
2016-11-20 10:06:51 +0700

    Initial commit

---
+++ app/src/main/java/com/example/whattoeat/Model/Food.java
@@ -0,0 +1,13 @@
+package com.example.whattoeat.Model;
+
+public class Food {
+
+    public final String name;
+    public final String pictureFileName;
+
+    public Food(String name, String pictureFileName) {
+        this.name = name;
+        this.pictureFileName = pictureFileName;
+    }
+    
+}
