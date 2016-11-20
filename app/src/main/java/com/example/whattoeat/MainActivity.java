4027cff4b0445e8cba5e5c2329118fc03ca9aa63
Wallop Opasakhun <champbassist159@gmail.com>
2016-11-20 12:09:04 +0700

    add view dialog

--- app/src/main/java/com/example/whattoeat/MainActivity.java
+++ app/src/main/java/com/example/whattoeat/MainActivity.java
@@ -1,35 +1,28 @@
 package com.example.whattoeat;
 
 import android.content.Intent;
+import android.content.res.AssetManager;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.example.whattoeat.Model.Food;
+import com.example.whattoeat.Model.FoodMenu;
+
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
-
-    private String[] mFood = {
-            "ข้าวไข่เจียว", "ข้าวหมูแดง", "ข้าวมันไก่",
-            "ข้าวหน้าเป็ด", "ข้าวผัด", "ผัดซีอิ๊ว",
-            "ผัดไทย", "ราดหน้า", "ส้มตำไก่ย่าง",
-    };
-
-    private int[] mImageId = {
-            R.drawable.kao_kai_jeaw,
-            R.drawable.kao_moo_dang,
-            R.drawable.kao_mun_kai,
-            R.drawable.kao_na_ped,
-            R.drawable.kao_pad,
-            R.drawable.pad_sie_eew,
-            R.drawable.pad_thai,
-            R.drawable.rad_na,
-            R.drawable.som_tum_kai_yang
-    };
 
     private Random mRandom = new Random();
 
@@ -38,46 +31,42 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button randomButton = (Button) findViewById(R.id.random_button);
+        final Button randomButton = (Button) findViewById(R.id.random_button);
         randomButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                int randomIndex = mRandom.nextInt(mFood.length);
-                String randomFood = mFood[randomIndex];
-                int foodImageId = mImageId[randomIndex];
-                Log.i(TAG, randomFood);
 
-                Intent intent = new Intent(MainActivity.this, DishDetailActivity.class);
-                intent.putExtra("dish_name", randomFood);
-                intent.putExtra("dish_image", foodImageId);
-                startActivity(intent);
+                FoodMenu foodMenu = FoodMenu.getInstance(MainActivity.this);
+                foodMenu.loadFromDatabase();
 
-/*
-                AlertDialog.Builder dialog = new AlertDialog.Builder(MainActivity.this);
-                dialog.setTitle("ชื่ออาหารที่สุ่มได้");
-                dialog.setIcon(R.mipmap.ic_launcher);
-                dialog.setMessage(randomFood);
-                dialog.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
+                Food randomFood = foodMenu.getRandomFood();
 
-                    }
-                });
-                dialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
+                AlertDialog.Builder alert = new AlertDialog.Builder(MainActivity.this);
+                LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+                View layout = inflater.inflate(R.layout.dialog_layout, null);
+                ImageView foodImage = (ImageView) layout.findViewById(R.id.food_image_view);
+                TextView foodName = (TextView) layout.findViewById(R.id.food_name_text_view);
 
-                    }
-                });
-                dialog.setNeutralButton("xxx", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
+                foodName.setText(randomFood.name);
+                Drawable drawable = getDrawableFromAssets(randomFood.pictureFileName);
+                foodImage.setImageDrawable(drawable);
 
-                    }
-                });
-                dialog.show();
-*/
+                alert.setView(layout);
+                alert.show();
             }
         });
     }
+
+    private Drawable getDrawableFromAssets(String pictureFileName) {
+        AssetManager am = getAssets();
+        try {
+            InputStream stream = am.open(pictureFileName);
+            Drawable drawable = Drawable.createFromStream(stream, null);
+            return drawable;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
 }
--- app/src/main/java/com/example/whattoeat/Model/FoodMenu.java
+++ app/src/main/java/com/example/whattoeat/Model/FoodMenu.java
@@ -1,16 +1,33 @@
 package com.example.whattoeat.Model;
 
 import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
 
-/**
- * Singleton pattern
- * design OOP pattern to make sure this class can be create only a single time.
- */
+import com.example.whattoeat.DB.databaseHelper;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 public class FoodMenu {
 
+    private String TAG = FoodMenu.class.getSimpleName();
+
+    private static databaseHelper helper;
+    private static SQLiteDatabase db;
+
+    private static Random mRandom = new Random();
+
+    /**
+     * Singleton pattern
+     * 2 following classes are OOP pattern to make sure this class can be create only a single time.
+     */
+
     private static FoodMenu mInstance;
-    private Context context;
+    private static Context context;
+
+    private static ArrayList<Food> mFoodList = new ArrayList();
 
     public static FoodMenu getInstance(Context context){
         if(mInstance == null){
@@ -21,6 +38,36 @@ public class FoodMenu {
 
     private FoodMenu(Context context) {
         this.context = context;
+        loadFromDatabase();
+    }
+
+    public static void loadFromDatabase() {
+
+        mFoodList.clear();
+        helper = new databaseHelper(context);
+        db = helper.getWritableDatabase();
+
+        Cursor cursor = db.query(databaseHelper.TABLE_NAME, null, null, null, null, null, null);
+
+        while(cursor.moveToNext()){
+            String name = cursor.getString(cursor.getColumnIndex(databaseHelper.COL_NAME));
+            String picture = cursor.getString(cursor.getColumnIndex(databaseHelper.COL_PICTURE));
+            mFoodList.add(new Food(name, picture));
+        }
+
+        cursor.close(); // unnecessary, but should
+
+    }
+
+    public ArrayList<Food> getFoodList() {
+        return mFoodList;
+    }
+
+    public Food getRandomFood(){
+        int randomIndex = mRandom.nextInt(mFoodList.size());
+        Food food = mFoodList.get(randomIndex);
+        Log.i(TAG, food.name + ", " + food.pictureFileName);
+        return food;
     }
 
 }
